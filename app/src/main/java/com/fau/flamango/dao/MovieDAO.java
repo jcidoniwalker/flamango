d65453110fff21c3f07aef9ff789e52f8706b492
Joshua Cidoni-Walker <32071430+jcidoniwalker@users.noreply.github.com>
2020-04-23 22:35:28 -0400

    This fix closes #5.

--- app/src/main/java/com/fau/flamango/dao/MovieDAO.java
+++ app/src/main/java/com/fau/flamango/dao/MovieDAO.java
@@ -53,4 +53,80 @@ public class MovieDAO {
         // Access the RequestQueue through your singleton class.
         MainActivity.getVolley().addToRequestQueue(jsonObjectRequest);
     }
+
+    public void getMovieByTitle(String title, final DataReceived dr) {
+        // https://api.themoviedb.org/3/search/movie?api_key=ff7c2e230c20460f35b90035e3b9ca11&language=en-US&query=texas%20chainsaw&page=1&include_adult=false
+        String url = api_base + "/search/movie?api_key=" + api_key + "&query=" + title;
+        System.out.println("requesting " + url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    ArrayList<Movie> tmp = new ArrayList<>();
+                    JSONArray json_array = response.getJSONArray("results");
+                    for(int i = 0; i < json_array.length(); i++) {
+                        String title = (String) json_array.getJSONObject(i).get("title");
+                        String description = (String) json_array.getJSONObject(i).get("overview");
+                        System.out.println(title);
+                        System.out.println(description);
+                        String uri = json_array.getJSONObject(i).getString("poster_path");
+                        System.out.println(uri);
+                        Movie movie = new Movie(title, description, "http://image.tmdb.org/t/p/w185///" + uri);
+                        tmp.add(movie);
+                    }
+                    dr.onDataReceived(tmp);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString());
+            }
+        });
+
+        // Access the RequestQueue through your singleton class.
+        MainActivity.getVolley().addToRequestQueue(jsonObjectRequest);
+    }
+
+
+    public void getMovieByPerson(String name, final DataReceived dr) {
+        // https://api.themoviedb.org/3/search/movie?api_key=ff7c2e230c20460f35b90035e3b9ca11&language=en-US&query=texas%20chainsaw&page=1&include_adult=false
+        String url = api_base + "/search/person?api_key=" + api_key + "&query=" + name;
+        System.out.println("requesting " + url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    ArrayList<Movie> tmp = new ArrayList<>();
+                    JSONArray json_array = response.getJSONArray("results").getJSONObject(0).getJSONArray("known_for");
+
+                    for(int i = 0; i < json_array.length(); i++) {
+                        String title = (String) json_array.getJSONObject(i).get("title");
+                        String description = (String) json_array.getJSONObject(i).get("overview");
+                        System.out.println(title);
+                        System.out.println(description);
+                        String uri = json_array.getJSONObject(i).getString("poster_path");
+                        System.out.println(uri);
+                        Movie movie = new Movie(title, description, "http://image.tmdb.org/t/p/w185///" + uri);
+                        tmp.add(movie);
+                    }
+                    dr.onDataReceived(tmp);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.toString());
+            }
+        });
+
+        // Access the RequestQueue through your singleton class.
+        MainActivity.getVolley().addToRequestQueue(jsonObjectRequest);
+    }
 }
--- app/src/main/java/com/fau/flamango/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/fau/flamango/ui/dashboard/DashboardFragment.java
@@ -4,23 +4,109 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.fau.flamango.Adapter;
 import com.fau.flamango.R;
+import com.fau.flamango.dao.MovieDAO;
+import com.fau.flamango.interfaces.DataReceived;
+import com.fau.flamango.models.Movie;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
+    private Adapter adapter;
+    private ArrayList<Movie> items;
+    private RecyclerView recyclerView;
+    private MovieDAO movieDAO;
+
+    private EditText search_box;
+    private RadioGroup radiogroup;
+    private RadioButton radio_movie;
+    private RadioButton radio_person;
+    private RadioButton radio_genre;
+    private Button search_button;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
 
+        search_box = root.findViewById(R.id.editText3);
+        radiogroup = root.findViewById(R.id.radiogroup1);
+        radio_movie = root.findViewById(R.id.radioButton);
+        radio_person = root.findViewById(R.id.radioButton2);
+        search_button = root.findViewById(R.id.button3);
+        search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch(radiogroup.getCheckedRadioButtonId()) {
+                    case R.id.radioButton: // movie title
+                        items.clear();
+                        displayMovies();
+                        break;
+
+                    case R.id.radioButton2: // director
+                        items.clear();
+                        displayPerson();
+                        break;
+                }
+            }
+        });
+
+        recyclerView = root.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
+        recyclerView.setAdapter(new Adapter(root.getContext(), new ArrayList<Movie>()));
+
+        items = new ArrayList<Movie>();
+
+        movieDAO = new MovieDAO();
+
         return root;
+    }
+
+    public void displayMovies() {
+        String title = search_box.getText().toString();
+        title = title.trim();
+        title.replaceAll("\\s", "%20");
+
+        movieDAO.getMovieByTitle(title, new DataReceived() {
+            @Override
+            public void onDataReceived(ArrayList<Movie> movies) {
+                for(Movie movie : movies) {
+                    items.add(movie);
+                    System.out.println(movie.getTitle());
+                }
+
+                adapter = new Adapter(getActivity().getApplicationContext(), items);
+                recyclerView.setAdapter(adapter);
+            }
+        });
+    }
+
+    public void displayPerson() {
+        String name = search_box.getText().toString();
+        name = name.trim();
+        name.replaceAll("\\s", "%20");
+
+        movieDAO.getMovieByPerson(name, new DataReceived() {
+            @Override
+            public void onDataReceived(ArrayList<Movie> movies) {
+                for(Movie movie : movies) {
+                    items.add(movie);
+                    System.out.println(movie.getTitle());
+                }
+
+                adapter = new Adapter(getActivity().getApplicationContext(), items);
+                recyclerView.setAdapter(adapter);
+            }
+        });
     }
 }
