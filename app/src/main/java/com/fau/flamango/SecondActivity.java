42545fab7e52e6a013a91fbbbf07f66bf6864471
Joshua Cidoni-Walker <32071430+jcidoniwalker@users.noreply.github.com>
2020-04-25 10:27:26 -0400

    This closes #11.

--- app/src/main/java/com/fau/flamango/SecondActivity.java
+++ app/src/main/java/com/fau/flamango/SecondActivity.java
@@ -7,7 +7,6 @@ import android.view.MenuItem;
 import com.fau.flamango.models.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
