7d26a8104964c7e3ff5007ec3ff83a8fa6b8aaea
Joshua Cidoni-Walker <32071430+jcidoniwalker@users.noreply.github.com>
2020-04-25 10:23:40 -0400

    This closes #10.

--- app/src/main/java/com/fau/flamango/SecondActivity.java
+++ app/src/main/java/com/fau/flamango/SecondActivity.java
@@ -2,10 +2,12 @@ package com.fau.flamango;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.fau.flamango.models.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -24,14 +26,24 @@ public class SecondActivity extends AppCompatActivity {
         user = (User) intent.getSerializableExtra("userobj");
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.getMenu().findItem(R.id.navigation_signout).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent i = new Intent(getApplication(), MainActivity.class);
+                startActivity(i);
+                return true;
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_signout).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    public void gotoMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     public static User getUser() {
